418195ea6e5c6218a9228a85ef86dcdecd39ac28
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2015-06-16 21:59:24 -0400

    Exclusive args.

--- wok/main.py
+++ wok/main.py
@@ -34,20 +34,21 @@ def main():
     parser.add_argument('-c', '--conf', nargs='?',
                         default=os.path.expanduser('~/.wok.yaml'),
                         help='yaml config file')
-    parser.add_argument('-i', '--install', nargs='*',
+    ex_main = parser.add_mutually_exclusive_group()
+    ex_main.add_argument('-i', '--install', nargs='*',
                         metavar='PROG', help='install specified programs')
-    parser.add_argument('-r', '--remove', nargs='*',
+    ex_main.add_argument('-r', '--remove', nargs='*',
                         metavar='PROG', help='remove specified programs')
-    parser.add_argument('-u', '--update', default=False, action='store_true',
+    ex_main.add_argument('-u', '--update', default=False, action='store_true',
                         help='update installed programs')
     args = parser.parse_args()
     logging.debug('CLI ARGS: %s', args)
 
-    install_dir = os.path.join(BUILD_DIR, Ag.__name__.lower())
-    build = Ag(install_dir)
-    build.download()
-    build.build()
-    assert build.verify()
+    #install_dir = os.path.join(BUILD_DIR, Ag.__name__.lower())
+    #build = Ag(install_dir)
+    #build.download()
+    #build.build()
+    #assert build.verify()
 
 if __name__ == '__main__':
     main()
