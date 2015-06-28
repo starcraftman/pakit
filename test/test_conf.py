364180cbb3bcd5bd874845e759d49da491417ffd
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2015-06-27 22:00:29 -0400

    Naming.

--- test/test_conf.py
+++ test/test_conf.py
@@ -14,28 +14,28 @@ class TestConfig(object):
         with open(WOK_FILE, 'w') as fout:
             fout.write(output)
 
-        self.co = Config(WOK_FILE)
+        self.conf = Config(WOK_FILE)
 
     def teardown(self):
         os.remove(WOK_FILE)
 
-    def test_get(self):
-        assert self.co.paths.get('prefix') == '/tmp/wok/builds'
-
     def test_filename(self):
         wok2 = './wok2.yaml'
-        self.co.filename = wok2
-        self.co.write()
+        self.conf.filename = wok2
+        self.conf.write()
         assert os.path.exists(wok2)
         os.remove(wok2)
 
+    def test_get(self):
+        assert self.conf.paths.get('prefix') == '/tmp/wok/builds'
+
     def test_set(self):
-        self.co.set('paths.prefix', '/dev/null')
-        assert self.co.paths.get('prefix') == '/dev/null'
+        self.conf.set('paths.prefix', '/dev/null')
+        assert self.conf.paths.get('prefix') == '/dev/null'
 
     def test_write(self):
-        self.co.set('paths.install', 22)
-        self.co.write()
-        self.co.load()
-        assert self.co.paths.get('install') == 22
+        self.conf.set('paths.install', 22)
+        self.conf.write()
+        self.conf.load()
+        assert self.conf.paths.get('install') == 22
 
