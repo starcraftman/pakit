9f45cf6371044f4a76e4ca3377b9093348b69ad1
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2015-08-01 23:04:34 -0400

    Adding working setup & manifest.

---
+++ setup.py
@@ -0,0 +1,108 @@
+"""Wok: A homebrew like application
+
+https://github.com/starcraftman/wok
+"""
+from __future__ import absolute_import, print_function
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages, Command
+import glob
+import shlex
+import subprocess
+
+class CleanCommand(Command):
+    """ Equivalent of make clean. """
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        eggs = glob.glob('*.egg-info')
+        cmd = 'rm -vrf ./build ./dist ' + ' '.join(eggs)
+        print('Executing: ' + cmd)
+        subprocess.call(shlex.split(cmd))
+
+MY_NAME = 'Jeremy Pallats / starcraft.man'
+MY_EMAIL = 'N/A'
+setup(
+    name='wok',
+    version='0.1.0',
+    description='A homebrew like application',
+    long_description='nothing',
+    url='https://github.com/starcraftman/wok',
+    author=MY_NAME,
+    author_email=MY_EMAIL,
+    maintainer=MY_NAME,
+    maintainer_email=MY_EMAIL,
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.2',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
+    ],
+
+    # What does your project relate to?
+    keywords='development',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # packages=find_packages(exclude=['venv', 'test*']),
+    packages=find_packages(exclude=['venv', 'tests*']),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['argparse', 'PyYAML'],
+
+    tests_require=['pytest', 'mock'],
+
+    # # List additional groups of dependencies here (e.g. development
+    # # dependencies). You can install these using the following syntax,
+    # # for example:
+    # # $ pip install -e .[dev,test]
+    # extras_require={
+        # 'dev': ['check-manifest'],
+        # 'test': ['coverage'],
+    # },
+
+    # # If there are data files included in your packages that need to be
+    # # installed, specify them here.  If using Python 2.6 or less, then these
+    # # have to be included in MANIFEST.in as well.
+    # package_data={
+        # 'sample': ['package_data.dat'],
+    # },
+
+    # # Although 'package_data' is the preferred approach, in some case you may
+    # # need to place data files outside of your packages. See:
+    # # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'wok = wok.main:main',
+        ],
+    },
+
+    cmdclass={
+        'clean': CleanCommand,
+    }
+)
