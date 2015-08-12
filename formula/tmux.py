2cb2dfc6de78df3452a952f54389189d40e2f389
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2015-08-11 21:31:54 -0400

    Added tmux recipe.

---
+++ formula/tmux.py
@@ -0,0 +1,22 @@
+from wok import *
+
+
+class Tmux(Recipe):
+    def __init__(self):
+        super(Tmux, self).__init__()
+        self.desc = 'The modern screen replacement'
+        self.src = 'https://github.com/tmux/tmux'
+        self.homepage = self.src
+        self.repos = {
+            'stable': Git(self.src, tag='2.0'),
+            'unstable': Git(self.src),
+        }
+
+    def build(self):
+        self.cmd('sh autogen.sh')
+        self.cmd('./configure --prefix {prefix}')
+        self.cmd('make install')
+
+    def verify(self):
+        lines = self.cmd('{link}/bin/tmux -V')
+        return lines[0].find('tmux') != -1
