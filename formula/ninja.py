1f47545c5cf46b73cc696ed15d6db32022255b01
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2015-08-16 10:17:40 -0400

    Formula for ninja.

---
+++ formula/ninja.py
@@ -0,0 +1,29 @@
+""" Formula for building ninja """
+import os
+import re
+
+from wok import Git, Recipe
+
+
+class Ninja(Recipe):
+    """ A small build system optimized for speed """
+    def __init__(self):
+        super(Ninja, self).__init__()
+        self.desc = 'A small build system optimized for speed'
+        self.src = 'https://github.com/martine/ninja.git'
+        self.homepage = self.src
+        self.repos = {
+            'stable': Git(self.src, tag='release'),
+            'unstable': Git(self.src),
+        }
+
+    def build(self):
+        self.cmd('./configure.py --bootstrap')
+        os.renames('{source}/ninja'.format(**self.opts),
+                   '{prefix}/bin/ninja'.format(**self.opts))
+
+    def verify(self):
+        lines = self.cmd('{link}/bin/ninja --version')
+        matcher = re.match('\d\.\d+\.\d+', lines[0])
+        return len(matcher.group()) == 1
+
