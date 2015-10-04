393947a3c4f8b9ff6032735eacd03ef2b84c0241
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2015-10-04 14:22:25 -0400

    Change digraph to use dicts, easier remove.

--- proto/graph.py
+++ proto/graph.py
@@ -27,6 +27,100 @@ class Vertex(object):
         return self.name + ' was visited: ' + str(self.was_visited)
 
 
+class DiGraph(object):
+    """
+    Repesents a directed graph with adjacency lists.
+
+    Attributes:
+        adj_list: The adjacency list, key is a vertex key from vertex_list.
+        vertex_list: Stores whether a vertex was visited, default False.
+    """
+    def __init__(self):
+        self.vertex_list = {}
+        self.adj_list = {}
+
+    def __str__(self):
+        msg = ['The vertex list has: ' + str(self.num_verts) + ' elements.']
+        msg += sorted(self.vertex_list.keys())
+        msg += ['The adjacency list is:']
+        msg += ['{0}: {1}'.format(key, ', '.join(self.adj_list[key])) for key
+                in sorted(self.adj_list)]
+        return '\n '.join(msg)
+
+    def __contains__(self, key):
+        return key in self.vertex_list
+
+    @property
+    def num_verts(self):
+        """
+        The number of vertices in the graph.
+        """
+        return len(self.vertex_list)
+
+    def add_vertex(self, key):
+        """
+        Add a vertex to the graph, optionally with data.
+        """
+        self.vertex_list[key] = False
+        self.adj_list[key] = []
+
+    def add_edge(self, key, depends_on):
+        """
+        Add an edge from key to the vert depends_on
+
+        Args:
+            key: The vertex name.
+            depends_on: A vertex name key depends on.
+        """
+        self.adj_list[key].append(depends_on)
+
+    def add_edges(self, key, depends_on_all):
+        """
+        Add an edge from key to all verts in depends_on
+
+        Args:
+            key: The vertex name.
+            depends_on: A list of vertex names key depends on.
+        """
+        self.adj_list[key].extend(depends_on_all)
+
+    def get_unvisited_adjacent(self, key):
+        """
+        Get first position connected to key that has NOT
+        been visited before.
+
+        Returns:
+            A position of a vertex in vertex_list, else None if not found.
+        """
+        for adj_key in self.adj_list[key]:
+            if not self.vertex_list[adj_key]:
+                return adj_key
+
+        return None
+
+    def is_connected(self, start, end):
+        """
+        Returns true iff start has an edge to end.
+        """
+        return end in self.adj_list[start]
+
+    def remove(self, key):
+        """
+        Remove a vertex from the graph.
+        Delete it from all adjacency lists as well.
+        """
+        try:
+            del self.vertex_list[key]
+            del self.adj_list[key]
+        except KeyError:
+            pass
+        for adj_key in self.adj_list:
+            try:
+                self.adj_list[adj_key].remove(key)
+            except ValueError:
+                pass
+
+
 class Graph(object):
     """
     Repesents an undirected graph with adjacency list.
@@ -104,17 +198,6 @@ class Graph(object):
                 pass
         del self.adj_list[v_index]
         del self.vertex_list[v_index]
-
-
-class DiGraph(Graph):
-    """
-    Directed graph.
-    """
-    def add_edge(self, start, end):
-        """
-        Start and end are positional indexes into vertex list.
-        """
-        self.adj_list[start].append(end)
 
 
 def bfs_search(graph, func, start_pos):
@@ -202,25 +285,17 @@ def topo_list(graph):
     Raises:
         CycleInGraphError: The directed graph had a cycle.
     """
-    # FIXME: Need to change adj_list to index on string keys.
-    #        When deleting, int positions become stale refs.
-    #        Works if I go from back for now.
     t_list = []
 
-    last_len = len(graph.vertex_list)
-    while len(graph.vertex_list) != 0:
-        print(graph)
-        for ind in range(graph.num_verts - 1, -1, -1):
-            vert = graph.vertex_list[ind]
-            adj_list = graph.adj_list[ind]
-
-            if len(adj_list) == 0:
-                print('Select:', vert, adj_list)
-                t_list.append(vert.name)
-                graph.remove(ind)
+    last_len = graph.num_verts
+    while graph.num_verts:
+        for key in graph.adj_list:
+            if graph.adj_list[key] == []:
+                print('Select:', key)
+                t_list.append(key)
+                graph.remove(key)
                 break
 
-        print(t_list)
         new_len = len(graph.vertex_list)
         if last_len == new_len:
             print(graph)
--- proto/test_graph.py
+++ proto/test_graph.py
@@ -65,19 +65,6 @@ class TestUndirectedGraph(object):
         assert 'C' not in self.g.vertex_list
 
 
-class TestDirectedGraph(object):
-    def setup(self):
-        self.g = graph.DiGraph(10)
-
-    def test_add_edge(self):
-        self.g.add_vertex('A')
-        self.g.add_vertex('B')
-        self.g.add_edge(0, 1)
-        assert self.g.is_connected(0, 1)
-        assert 1 in self.g.adj_list[0]
-        assert 0 not in self.g.adj_list[1]
-
-
 class TestSearchGraph(object):
     def setup(self):
         max_verts = 10
@@ -128,19 +115,75 @@ class TestMST(object):
 class TestTopologicalSort(object):
     def setup(self):
         max_verts = 8
-        self.g = graph.DiGraph(max_verts)
+        self.g = graph.DiGraph()
         for char in string.ascii_uppercase[0:max_verts]:
             self.g.add_vertex(char)
 
-        self.g.add_edge(0, 3)
-        self.g.add_edge(0, 4)
-        self.g.add_edge(1, 4)
-        self.g.add_edge(2, 5)
-        self.g.add_edge(3, 6)
-        self.g.add_edge(4, 6)
-        self.g.add_edge(5, 7)
-        self.g.add_edge(6, 7)
+        self.g.add_edge('A', 'D')
+        self.g.add_edge('A', 'E')
+        self.g.add_edge('B', 'E')
+        self.g.add_edge('C', 'F')
+        self.g.add_edge('D', 'G')
+        self.g.add_edge('E', 'G')
+        self.g.add_edge('F', 'H')
+        self.g.add_edge('G', 'H')
 
     def test_topo_sort(self):
         print(self.g)
-        print(graph.topo_list(self.g))
+        expect = ['H', 'G', 'E', 'B', 'D', 'A', 'F', 'C']
+        assert graph.topo_list(self.g) == expect
+
+
+class TestDiGraph(object):
+    def setup(self):
+        self.g = graph.DiGraph()
+
+    def test_num_verts(self):
+        self.g.add_vertex('A')
+        self.g.add_vertex('B')
+        assert self.g.num_verts == 2
+
+    def test_add_vertex(self):
+        self.g.add_vertex('A')
+        assert 'A' in self.g
+
+    def test_add_edge(self):
+        self.g.add_vertex('A')
+        self.g.add_vertex('B')
+        self.g.add_edge('A', 'B')
+        assert self.g.is_connected('A', 'B')
+        print(self.g)
+
+    def test_is_connected(self):
+        self.g.add_vertex('A')
+        self.g.add_vertex('B')
+        self.g.add_vertex('C')
+        self.g.add_edge('A', 'B')
+        print(self.g)
+        assert self.g.is_connected('A', 'B')
+        assert not self.g.is_connected('A', 'C')
+
+    def test_get_unvisited_adjacent(self):
+        self.g.add_vertex('A')
+        self.g.add_vertex('B')
+        self.g.add_vertex('C')
+        self.g.add_edges('A', ['B', 'C'])
+        assert self.g.get_unvisited_adjacent('A') == 'B'
+        self.g.vertex_list['B'] = True
+        assert self.g.get_unvisited_adjacent('A') == 'C'
+        self.g.vertex_list['C'] = True
+        assert self.g.get_unvisited_adjacent('A') is None
+
+    def test_remove(self):
+        self.g.add_vertex('A')
+        self.g.add_vertex('B')
+        self.g.add_vertex('C')
+        self.g.add_edges('A', ['B', 'C'])
+        print(self.g)
+        assert self.g.num_verts == 3
+        self.g.remove('C')
+        print(self.g)
+        assert self.g.num_verts == 2
+        assert 'C' not in self.g
+        assert self.g.is_connected('A', 'B')
+        assert not self.g.is_connected('A', 'C')
